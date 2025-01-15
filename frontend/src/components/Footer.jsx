e65c677cbbf8c17d14d937ccc8a30755c1b80d9b
Hasini Sankalpana <156077759+Hasini-Sankalpana@users.noreply.github.com>
2025-01-15 15:27:04 +0530

    make footer section responsive

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,13 +6,25 @@ const Footer = () => {
   return (
     <footer className="bg-[#16051f] text-white py-8 mt-10 font-roboto">
       <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h3 className="text-3xl text-center mb-6 font-lemon font-extralight text-purple-600">StarX</h3>
-        <nav className="flex justify-center space-x-6 mb-6">
-          <a href="#hero" className="hover:text-[#9333ea]">Home</a>
-          <a href="#blogs" className="hover:text-[#9333ea]">Articles</a>
-          <a href="#news" className="hover:text-[#9333ea]">News</a>
-          <a href="#quize" className="hover:text-[#9333ea]">Quizzes</a>
-          <a href="#gallery" className="hover:text-[#9333ea]">Gallery</a>
+        <h3 className="text-3xl text-center mb-6 font-lemon font-extralight text-purple-600">
+          StarX
+        </h3>
+        <nav className="flex flex-wrap justify-center space-x-6 mb-6">
+          <a href="#hero" className="hover:text-[#9333ea] mb-2 sm:mb-0">
+            Home
+          </a>
+          <a href="#blogs" className="hover:text-[#9333ea] mb-2 sm:mb-0">
+            Articles
+          </a>
+          <a href="#news" className="hover:text-[#9333ea] mb-2 sm:mb-0">
+            News
+          </a>
+          <a href="#quize" className="hover:text-[#9333ea] mb-2 sm:mb-0">
+            Quizzes
+          </a>
+          <a href="#gallery" className="hover:text-[#9333ea] mb-2 sm:mb-0">
+            Gallery
+          </a>
         </nav>
         <div className="flex justify-center space-x-6 mb-6">
           <a
@@ -32,7 +44,9 @@ const Footer = () => {
             <FaGithub />
           </a>
         </div>
-        <p className="text-center text-sm opacity-70">© 2025 StartX. All rights Reserved.</p>
+        <p className="text-center text-sm opacity-70">
+          © 2025 StartX. All rights Reserved.
+        </p>
       </div>
     </footer>
   );
