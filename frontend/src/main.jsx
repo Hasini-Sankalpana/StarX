519e64655c999a34117252be938db2fd445234cd
Hasini Sankalpana <156077759+Hasini-Sankalpana@users.noreply.github.com>
2025-01-15 13:53:47 +0530

    updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,6 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable Key');
-}
 
 createRoot(document.getElementById('root')).render(
   
